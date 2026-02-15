a2a545e8f486fdd73cb86bc3c0b1913a9124c616
Ujjwal Gupta <iamujjwalgupta56@gmail.com>
2026-02-15 18:05:43 +0530

    Initial commit - Snake Gun Game

---
+++ game.py
@@ -0,0 +1,39 @@
+'''
+1 for snake
+-1 for water
+0 for gun
+'''
+import random
+
+computer = random.choice([-1, 0, 1])
+youstr = input("Enter your choice: ")
+youDict = {"s":  1, "g": 0, "w": -1}
+reverseDict = {1: "Snake", -1: "Water", 0: "Gun"}
+you = youDict[youstr]
+
+print(f"You Choose {reverseDict[you]}\nComputer Choose {reverseDict[computer]}")
+
+if (computer == you):
+    print("It's a Draw!")
+
+else:
+    if (computer == -1 and you == 1):
+        print("You Win!")
+
+    elif (computer == 1 and you == 0):
+        print("You Lose!")
+
+    elif (computer == 1 and you == -1):
+        print("You Lose!")
+
+    elif (computer == 1 and you == 0):
+        print("You Win!")
+
+    elif (computer == 1 and you == -1):
+        print("You Win!")
+
+    elif (computer == 1 and you == 0):
+        print("You Lose!")
+
+    else:
+        print("Something Went Wrong!")
